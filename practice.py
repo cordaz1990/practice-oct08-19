52f7c958e64032f1c0800e258e5651c25cc9cb89
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 14:05:20 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,13 @@
-food_words =["Yellow", "mango", "c
+s = 'c3H7'
+total = 0 #The sum of the digits seen so far.
+count = 0 #The number of digits seen so far.
+for i in range(len(s)):
+  if s[i].ialpha():
+    continue
+  total = total + int(s[i])
+  count = count + 1
+  
+total
+10
+count
+2
